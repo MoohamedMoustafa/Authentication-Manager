83f87e98aee27fb9e5f7d8a4d3e548890e177492
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-05-28 19:15:19 +0300

    implement signup functionality

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import AuthContent from "../components/auth/AuthContent";
+import { useSignupMutation } from "../services/authService";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [signup, { error, isLoading }] = useSignupMutation();
+  function submitHandler(credentials) {
+    signup(credentials);
+  }
+  if (isLoading) return <LoadingOverlay message="Creating user..." />;
+  return <AuthContent onAuthenticate={submitHandler} />;
 }
 
 export default SignupScreen;
--- services/authService.js
+++ services/authService.js
@@ -1,19 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = "https://identitytoolkit.googleapis.com/v1/accounts:";
-const SIGNUP = "signUp?key=";
-const LOGIN = "signInWithPassword?key=";
+const BASE_URL = "https://identitytoolkit.googleapis.com/v1/accounts";
+const SIGNUP = ":signUp?key=";
+const LOGIN = ":signInWithPassword?key=";
 const API_KEY = "AIzaSyBMmU8m5Sq5dw5kAKqniX0daYjncgDBLe0";
 export const authService = createApi({
   reducerPath: "authService",
-  baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}${API_KEY}` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}` }),
   tagTypes: ["Auth"],
   endpoints: (builder) => ({
     signup: builder.mutation({
       query: (credentials) => ({
         url: `${SIGNUP}${API_KEY}`,
         method: "POST",
-        body: credentials,
+        body: {...credentials, returnSecureToken: true},
         headers: {
           "Content-Type": "application/json",
         },
