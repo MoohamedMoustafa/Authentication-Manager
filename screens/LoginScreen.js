5b967563c31cdbc8b3fcfe6b0923b72de968ec3b
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-05-30 16:36:37 +0300

    handling errors of login and signup

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,18 +1,29 @@
-import { useLoginMutation } from '../services/authService';
-import AuthContent from '../components/auth/AuthContent';
-import LoadingOverlay from '../components/UI/LoadingOverlay';
+import { useLoginMutation } from "../services/authService";
+import AuthContent from "../components/auth/AuthContent";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  const [login, {isLoading, error, data}] = useLoginMutation();
+  const [login, { isLoading, error, data }] = useLoginMutation();
   async function submitHandler(credentials) {
-    const res =await login(credentials);
-
-    console.log("response from login",res)
-    console.log("data from login",data);
-    
+    try {
+      const res = await login(credentials);
+      console.log("res.error.data.error.message", res.error.data.error.message);
+      if (res.error.data.error.message === "EMAIL_NOT_FOUND") {
+        throw new Error("this email is not registered");
+      }
+      if (
+        res.error.data.error.message === "INVALID_PASSWORD" ||
+        res.error.data.error.message === "INVALID_LOGIN_CREDENTIALS"
+      ) {
+        throw new Error("this email or password is not correct");
+      }
+    } catch (error) {
+      Alert.alert("Login failed", error.message);
+    }
   }
-  if(isLoading) {
-    return <LoadingOverlay message="Logging in..." />
+  if (isLoading) {
+    return <LoadingOverlay message="Logging in..." />;
   }
   return <AuthContent isLogin onAuthenticate={submitHandler} />;
 }
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,11 +2,22 @@ import { useEffect } from "react";
 import AuthContent from "../components/auth/AuthContent";
 import { useSignupMutation } from "../services/authService";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { Alert } from "react-native";
 
 function SignupScreen() {
   const [signup, { error, isLoading }] = useSignupMutation();
-  function submitHandler(credentials) {
-    signup(credentials);
+  async function submitHandler(credentials) {
+    try {
+      const res = signup(credentials).unwrap();
+      if (res.error.data.error.message === "EMAIL_EXISTS") {
+        throw new Error("this email is already registered");
+      }
+      if (res.error.data.error.message === "INVALID_EMAIL") {
+        throw new Error("this email is not valid");
+      }
+    } catch (error) {
+      Alert.alert("Signup failed", error.message);
+    }
   }
   if (isLoading) return <LoadingOverlay message="Creating user..." />;
   return <AuthContent onAuthenticate={submitHandler} />;
